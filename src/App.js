3ebf2125cebf1a73f55bc8fd8ef5d4db6f1895df
GwSrLeadMadOwl <gwsrmadowl@gmail.com>
2021-05-09 20:27:26 +0300

    adding mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import Card from "./components/card";
+import Card from "./components/card.jsx";
 
 import "./style.scss";
 
