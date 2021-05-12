916c9562a4cffe68b09c1fd20995c7d79d1999a5
GwSrLeadMadOwl <gwsrmadowl@gmail.com>
2021-05-12 14:56:12 +0300

    trying to add swipe function to scroll mouse

--- src/App.js
+++ src/App.js
@@ -10,27 +10,37 @@ import LIST_DATA from "./data";
 const App = () => {
     const [list, setList] = useState({
         bikes: LIST_DATA,
-        currentIndex: 0,
-        movement: 0
+        movement: 0,
     });
 
     const devider = 250;
+    const limiter = 10;
 
     let [start, setStart] = useState(0);
     let end = 0;
     let delta = 0;
 
+    let [duration, setDuration] = useState(0.5);
 
     const handleWheel = (e) => {
+        let scrollEnds;
         if (window.innerWidth > 1024) {
-            const wheelTimeout = setTimeout(() => handleMovementEnd(), 100);
-            clearTimeout(wheelTimeout);
-            handleMovement(e.deltaY || e.deltaX);
+            handleMovement(e.deltaY / limiter || e.deltaX / limiter);
+
+            //BUG WITH SWIPES
+            setDuration(1);
+            clearTimeout(scrollEnds);
+            scrollEnds = setTimeout((e) => {
+                // if (e.deltaX === 0 && e.deltaY === 0)
+                handleTouchEnd(); console.log("MUST FIT!");
+            }, 500);
         }
+        console.log(e.deltaX, e.deltaY);
     };
 
     const handleTouchStart = (e) => {
         start = Math.round(e.nativeEvent.touches[0].clientX);
+        setDuration(0.5);
     };
 
     const handleTouchMove = (e) => {
@@ -51,48 +61,6 @@ const App = () => {
         }
     };
 
-    const handleTouchEnd = () => {
-        handleMovementEnd();
-        end = 0;
-    };
-
-    const handleMovementEnd = () => {
-        const { bikes, movement } = list;
-
-        const endPosition = movement / 100;
-        const endPartial = endPosition % 1;
-        console.log(endPartial, "%");
-
-        const endingIndex = endPosition - endPartial;
-        console.log(endingIndex, "endingIndex");
-
-        let nextIndex = endingIndex;
-
-        if (start < window.innerWidth / 2 && endPartial > 0.1) {
-            // if (endPartial > 0.2) {
-            nextIndex++;
-            if (nextIndex > bikes.length)
-                nextIndex = 0;
-        }
-        // if (start > window.innerWidth / 2 && endPartial > 0.2) {
-        else if (endPartial > 0.1) {
-            nextIndex -= 0;
-            if (nextIndex < 0)
-                nextIndex = bikes.length - 2;
-        }
-
-        transitionTo(nextIndex);
-    };
-
-    const transitionTo = (index) => {
-        console.log(index);
-        setList({
-            bikes: LIST_DATA,
-            currentIndex: index,
-            movement: index * 100,
-        });
-    };
-
     const handleMovement = (delta) => {
         let nextMovement;
         setList((list) => {
@@ -102,17 +70,60 @@ const App = () => {
             //infinite scroll
             if (nextMovement < 0) {
                 nextMovement = maxLength * 100;
+                // duration = 0;
+                setDuration(0);
             }
             if (nextMovement > maxLength * 100) {
                 nextMovement = 0;
+                // duration = 0;
+                setDuration(0);
             }
 
             return {
                 bikes: LIST_DATA,
                 movement: nextMovement,
             };
-
         });
+    };
+
+    const handleTouchEnd = () => {
+        const { bikes, movement } = list;
+        fitPhoto(bikes, movement);
+        end = 0;
+    };
+
+    const fitPhoto = (bikes, movement) => {
+        const endPosition = movement / 100;
+        const endPartial = endPosition % 1;
+        const endingIndex = endPosition - endPartial;
+        let nextIndex = endingIndex;
+
+        if (start < window.innerWidth / 2 && endPartial > 0.1) {
+            nextIndex++;
+            if (nextIndex > bikes.length) {
+                nextIndex = -1;
+            }
+        }
+        else if (endPartial > 0.1) {
+            nextIndex -= 0;
+            if (nextIndex < 0) {
+                nextIndex = bikes.length - 2;
+            }
+        }
+        transitionTo(nextIndex);
+    };
+
+    const transitionTo = (index) => {
+        setList({
+            bikes: LIST_DATA,
+            currentIndex: index,
+            movement: index * 100
+        });
+    };
+
+    //select specific bike
+    const handleChange = (e) => {
+        transitionTo(e.target.value - 1);
     };
 
     return (
@@ -123,15 +134,15 @@ const App = () => {
                 onTouchMove={handleTouchMove}
                 onTouchEnd={handleTouchEnd}>
                 <div className="card-section">
-                    <div className="scroll" style={{ transform: `translateX(${list.movement * -1}%)` }}>
+                    <div className="scroll" style={{ transform: `translateX(${list.movement * -1}%)`, transitionDuration: `${duration}s` }}>
                         <Card bikes={list.bikes} />
                     </div>
                 </div>
-                {/* <select name="bikes" id="bikes">
+                <select name="bikes" id="bikes" onChange={handleChange}>
                     {list.bikes.map((bike) => (
-                        <option value={bike.name}>{bike.name.toUpperCase()}</option>
+                        <option value={bike.id} key={bike.id}>{bike.name.toUpperCase()}</option>
                     ))}
-                </select> */}
+                </select>
             </div>
         </>
     );
